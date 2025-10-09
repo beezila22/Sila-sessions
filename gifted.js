12cbd3ab0abe02508acf9dda171b0c2a8b49ef94
silatrix2 <silatrix22@gmail.com>
2025-10-09 15:10:26 +0300

    Add files via upload

---
+++ gifted.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 8000;
+let code = require('./pair');
+require('events').EventEmitter.defaultMaxListeners = 500;
+
+app.use('/code', code);
+
+// Serve 'pair.html' as the default page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pair.html'));
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`
+Pairing Server Running on http://localhost:` + PORT)
+});
+
+module.exports = app;
